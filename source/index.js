73e5b9186ee58b654e17139959256f004b992faa
Pedro Romão <prcd20020@gmail.com>
2022-10-01 09:51:32 -0300

    Moon rotating
    Created a first file with flask, a html file for visualizing what is going on with my changes in index.js which I'm using three.js to create the moon

---
+++ source/index.js
@@ -0,0 +1,44 @@
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+new THREE.TextureLoader().load("../img/background.jpeg", function (texture) {
+    scene.background = texture;
+})
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement)
+controls.update();
+
+const geometry = new THREE.SphereGeometry(5, 50, 50)
+const textureImage = '../img/color.jpg';
+const elevationMap = '../img/elevation.jpg';
+const material = new THREE.MeshStandardMaterial({
+    map: new THREE.TextureLoader().load(textureImage),
+    displacementMap: new THREE.TextureLoader().load(elevationMap)
+    // color: 0xffffff
+});
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.AmbientLight(color, intensity);
+scene.add(light);
+
+camera.position.z = 10;
+
+
+function animate() {
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+
+};
+
+animate();
+document.body.appendChild(renderer.domElement);
